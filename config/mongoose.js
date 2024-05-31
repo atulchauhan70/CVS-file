003a476d98e28ad701a3f3ed35393f6b1c20ef94
atulchauhan70 <98715672+atulchauhan70@users.noreply.github.com>
2024-05-31 12:40:09 +0530

    mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -8,23 +8,30 @@ dotenv.config();
 /** ------------------ MAKING CONNECTION ------------------ **/
 
 const DB = process.env.DB;
+const connectWithRetry = () => {
+    mongoose.connect(DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
+        socketTimeoutMS: 45000 // Increase socket timeout to 45 seconds
+    }).then(() => {
+        console.log('Connection successful!');
+    }).catch((err) => {
+        console.error("No connection: " + err);
+        console.log("Retrying connection in 5 seconds...");
+        setTimeout(connectWithRetry, 5000); // Retry connection after 5 seconds
+    });
+};
 
-mongoose.connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log('Connection successful!');
-}).catch((err) => {
-    console.log("No connection: " + err);
-});
+connectWithRetry();
 
-//setting it to db
+// Setting it to db
 const db = mongoose.connection;
 
 /** ------------------ CHECKING CONNECTION ------------------ **/
-//if error occurs
+// If an error occurs
 db.on("error", console.error.bind(console, "Error connecting to DB"));
-// when db connects successfully
+// When db connects successfully
 db.once("open", function(){
     console.log("Successfully connected to DB");
 });
